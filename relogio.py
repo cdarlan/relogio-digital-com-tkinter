0bb9a8a35e2c1a8386781f93ef9b9e627b05ff79
cdarlan <cdarlan@gmail.com>
2021-09-25 22:14:12 -0300

    initial project

---
+++ relogio.py
@@ -0,0 +1,24 @@
+from tkinter import *
+import tkinter
+
+
+############ cores ############
+cor1 = "#3d3d3d"  # preta
+cor2 = "#fafcff"  # branca
+cor3 = "#21c25c"  # verde
+cor4 = "#eb463b"  # vermelha
+cor5 = "#dedcdc"  # cinza
+cor6 = "#3080f0"  # azul
+
+fundo = cor1
+cor = cor2
+
+janela = Tk()
+janela.title("")
+janela.geometry("440x180")
+janela.resizable(width=FALSE, height=FALSE)
+janela.configure(bg=cor1)
+
+
+
+janela.mainloop()
